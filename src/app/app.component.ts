875a24ade4914dd5d5b5089c62f9ddb5bc63c262
Linguagem de Prog. para Web <lpweb.pmw@labin.canoas>
2017-09-02 15:11:19 -0300

    calculadora com elementos básicos de bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,98 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
-  aparece: string;
-  resultado: number;
-  operacao: string;
+  
   
   constructor(){
-    this.title = "Calculadora Web da Mari =)"
-    this.aparece='';
-    this.resultado=0
-    this.operacao='+';
+    
 
   }
-  
-  clicou(){
-    this.title='Aplicativo LPWEB Interativo'
+  ngOnInit() {
   }
   
-
-  um(){
-    this.aparece+=1;
-  }
-  dois(){
-    this.aparece+=2;
-  }
-  tres(){
-    this.aparece+=3;
-  }
-  quatro(){
-    this.aparece+=4;
-  }
-  cinco(){
-    this.aparece+=5;
-  }
-  seis(){
-    this.aparece+=6;
-  }
-  sete(){
-    this.aparece+=7;
-  }
-  
-  oito(){
-    this.aparece+=8;
-  }
-  nove(){
-    this.aparece+=9;
-  }
-  zero(){
-    this.aparece+=0;
-  }
-  menos(){
-    this.aparece = '-'
-  }
-  
- //operações
-
- somar()
- {
-   if(this.aparece != ''){
-     this.operacao = '+';
-     this.resultado = this.resultado+Number(this.aparece);
-     this.aparece = '';
-   }
- }
- subtrair(){
-   if(this.aparece != ''){
-     this.operacao = '-';
-     this.resultado = Number(this.aparece);
-     this.aparece = '';
-   }
-   else{
-     this.aparece = '-'
-   }
- }
- igual(){
-  if(this.operacao == '-')
-    {
-        this.resultado = this.resultado-Number(this.aparece);
-    }
-    else
-    {
-        this.resultado = this.resultado+Number(this.aparece);
-    }
-    this.aparece = '';
-    this.aparece = ''+this.resultado;
-    this.resultado = 0;
- }
- limpar(){
-  this.aparece ='';
-  this.resultado = 0;
-  this.operacao = '+';
- }
- 
   
   
 }
--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
+  title: string;
+  aparece: string;
+  resultado: number;
+  operacao: string;
 
   constructor() { 
 
@@ -14,5 +18,78 @@ export class CalculadoraComponent implements OnInit {
 
   ngOnInit() {
   }
+  um(){
+    this.aparece+=1;
+  }
+  dois(){
+    this.aparece+=2;
+  }
+  tres(){
+    this.aparece+=3;
+  }
+  quatro(){
+    this.aparece+=4;
+  }
+  cinco(){
+    this.aparece+=5;
+  }
+  seis(){
+    this.aparece+=6;
+  }
+  sete(){
+    this.aparece+=7;
+  }
+  
+  oito(){
+    this.aparece+=8;
+  }
+  nove(){
+    this.aparece+=9;
+  }
+  zero(){
+    this.aparece+=0;
+  }
+  menos(){
+    this.aparece = '-'
+  }
+
+  //operações
+
+ somar()
+ {
+   if(this.aparece != ''){
+     this.operacao = '+';
+     this.resultado = this.resultado+Number(this.aparece);
+     this.aparece = '';
+   }
+ }
+ subtrair(){
+   if(this.aparece != ''){
+     this.operacao = '-';
+     this.resultado = Number(this.aparece);
+     this.aparece = '';
+   }
+   else{
+     this.aparece = '-'
+   }
+ }
+ igual(){
+  if(this.operacao == '-')
+    {
+        this.resultado = this.resultado-Number(this.aparece);
+    }
+    else
+    {
+        this.resultado = this.resultado+Number(this.aparece);
+    }
+    this.aparece = '';
+    this.aparece = ''+this.resultado;
+    this.resultado = 0;
+ }
+ limpar(){
+  this.aparece ='';
+  this.resultado = 0;
+  this.operacao = '+';
+ }
 
 }
